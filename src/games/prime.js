058366455a3bcab8fb04364905244cf07f16c51e
Artem Smirnov <frees.comp@gmail.com>
2024-07-05 18:14:39 +0300

    add prime game

---
+++ src/games/prime.js
@@ -0,0 +1,26 @@
+import getRandomNumber from "../utils.js";
+import gameEngine from "../engine.js";
+
+const rules = `Answer "yes" if given number is prime. Otherwise answer "no".`;
+
+const isPrime = (number) => {
+    if (number < 2) {
+        return false;
+    }
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
+};
+
+const getQuesionAndAnswer = () => {
+    const randomNumber = getRandomNumber();
+    const question = `Question: ${randomNumber}`;
+    const answer = isPrime(randomNumber)? "yes" : "no";
+    return [question, answer];
+};
+
+export default () => gameEngine(getQuesionAndAnswer, rules);
+
